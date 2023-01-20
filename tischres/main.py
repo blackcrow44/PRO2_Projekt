f2391729e2d839240e8eafd38467672da3da5d12
blackcrow44 <113971346+blackcrow44@users.noreply.github.com>
2023-01-20 16:38:15 +0100

    Fehlermeldung aktualisiert

--- tischres/main.py
+++ tischres/main.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 # JSON importieren
 import json
 
-
 # Homepage
 @app.route('/', methods=["GET", "POST"])
 def homepage():
@@ -20,23 +19,31 @@ def homepage():
     elif request.method == "POST":
         return new_reservation(request.form)
 
+#Eingabe von neue Reservierungen
 def new_reservation(form):
     for field in form.keys():
+        # Wenn ein oder mehrere Felder leer sind, kommt eine Fehlermeldung.
         if form[field] == "":
             return render_template("error.html", title="Fehlermeldung", nachricht="Felder dürfen nicht leer sein.")
+        # Wenn die Telefonnummer keine Zahl ist, kommt eine Fehlermeldung.
         try:
             telefonnummer = int(form["Telefonnummer"])
         except ValueError:
             return render_template("error.html", title='Fehlermeldung', nachricht="Telefonnummer ist ungültig.")
+        # Wenn Anzahl Personen keine Zahl st
+        try:
+            anzahl = int(form["Anzahl Personen"])
+        except ValueError:
+            return render_template("error.html", title='Fehlermeldung', nachricht="Anzahl Personen kann nur aus Zahlen bestehen.")
 
-     #   newRes = {
-    #      "Name": reservation["Name"],
-     #       "Anzahl Personen": reservation["Anzahl Personen"],
-      #      "Datum": reservation.strftime("%d.%m.%Y")
-       #     "Uhrzeit": reservation["Uhrzeit"],
-        #    "Telefonnummer": reservation["Telefonnummer"],
-        #}
-        #reservationen.append(newRes)
+#   newRes = {
+#      "Name": reservation["Name"],
+#       "Anzahl Personen": reservation["Anzahl Personen"],
+#      "Datum": reservation.strftime("%d.%m.%Y")
+#     "Uhrzeit": reservation["Uhrzeit"],
+#    "Telefonnummer": reservation["Telefonnummer"],
+#       }
+#   reservationen.append(newRes)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
